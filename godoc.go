e8d83789a72bff2027f47f3ff8b17e4f0531d85a
h5law <dev@h5law.com>
2024-05-18 08:30:30 +0100

    chore: update godoc comment

--- godoc.go
+++ godoc.go
@@ -1,5 +1,7 @@
 // Package aks provides an implementation of the deterministic AKS Primality
-// Test, as well as the Miller-Rabin probabilistic primality test.
+// Test, as well as the Miller-Rabin probabilistic primality test. The library
+// also provides prime number generators such as the Sieve of Eratosthenes as
+// well as more modern algorithms.
 //
 //	Ref: https://en.wikipedia.org/wiki/AKS_primality_test
 //	Ref: https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
